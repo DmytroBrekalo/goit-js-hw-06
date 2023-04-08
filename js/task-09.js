f0a2e6b6a8f9546b8e00f6648056e1336285f3ee
DmytroBrekalo <brekalo4@gmail.com>
2023-04-08 15:14:51 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,16 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const buttonChanger = document.querySelector(".change-color");
+const bodyEl = document.querySelector("body");
+const spanTextColor = document.querySelector(".color");
+
+function handleChangeColor() {
+  const color = getRandomHexColor();
+  bodyEl.style.backgroundColor = color;
+  spanTextColor.textContent = color;
+  console.log(color);
+}
+
+buttonChanger.addEventListener("click", handleChangeColor);
