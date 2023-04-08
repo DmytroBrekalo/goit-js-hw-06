004886ab3de73a40e2b1df3a41689e8909a9faff
DmytroBrekalo <brekalo4@gmail.com>
2023-04-08 15:10:12 +0300

    Update task-06.js

---
+++ js/task-06.js
@@ -0,0 +1,12 @@
+const inputEl = document.getElementById("validation-input");
+const enumerateEl = inputEl.getAttribute("data-length");
+
+inputEl.addEventListener("blur", function () {
+  if (this.value.length != enumerateEl) {
+    this.classList.add("invalid");
+    this.classList.remove("valid");
+  } else {
+    this.classList.add("valid");
+    this.classList.remove("invalid");
+  }
+});
